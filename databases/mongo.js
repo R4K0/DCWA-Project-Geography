28007f0a946ce54f6b5fec5f8c623a54a9b42c4a
rako <milosz@rako.dev>
2021-01-04 02:49:59 +0000

    added settings for mongo

--- databases/mongo.js
+++ databases/mongo.js
@@ -1,13 +1,14 @@
 var mongoose = require("mongoose")
+var settings = require("../configs/mongo.json")
 
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "headsOfStateDB" }, () => {
+mongoose.connect(`mongodb://localhost:${settings.port}`, { useNewUrlParser: true, useUnifiedTopology: true, dbName: settings.database }, () => {
     console.log("MongoDB successfully connected!");
 })
 
 const HeadSchema = new mongoose.Schema({
     _id: { type: String },
     headOfState: { type: String, required: true }
-}, {collection: "headsOfState"})
+}, {collection: settings.database})
 
 const HeadModel = mongoose.model( "head", HeadSchema )
 
