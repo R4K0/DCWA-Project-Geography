b89ac712427fc0bb26cb6ae6846103e03fe4cc96
rako <milosz@rako.dev>
2021-01-04 02:52:31 +0000

    formatting

--- databases/mongo.js
+++ databases/mongo.js
@@ -8,9 +8,9 @@ mongoose.connect(`mongodb://localhost:${settings.port}`, { useNewUrlParser: true
 const HeadSchema = new mongoose.Schema({
     _id: { type: String },
     headOfState: { type: String, required: true }
-}, {collection: settings.database})
+}, { collection: settings.database })
 
-const HeadModel = mongoose.model( "head", HeadSchema )
+const HeadModel = mongoose.model("head", HeadSchema)
 
 module.exports = {
     HeadModel,
